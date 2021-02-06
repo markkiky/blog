373443dcd555d69c7a14f4b24fd29663f6d35c79
Mark Kariuki <markkaris438@gmail.com>
2021-02-06 15:14:38 +0300

    adds customer complains

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -15,6 +15,7 @@ class ReportsController < ApplicationController
 
   def complaints_reports
     begin
+      # get all tickets
       url = URI("https://bookinn.ngrok.io/tickets")
 
       http = Net::HTTP.new(url.host, url.port)
@@ -25,11 +26,30 @@ class ReportsController < ApplicationController
       response = http.request(request)
       puts response.read_body
 
-      @response_json = JSON.parse(response.read_body)
+      @response_tickets = JSON.parse(response.read_body)
+
+      # get all rooms
+      url = URI("https://bookinn.ngrok.io/room/status")
+
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      request = Net::HTTP::Post.new(url)
+      request["Authorization"] = "Bearer #{session["access_token"]}"
+      form_data = [["status", "2"]]
+      request.set_form form_data, "multipart/form-data"
+      # byebug
+      response = http.request(request)
+      puts response.read_body
+      @response_rooms = JSON.parse(response.read_body)
     rescue => exception
+      @rooms = {}
+      @tickets = []
     else
-      if @response_json["status"] == 200
+      if @response_tickets["status"] == 200 && @response_rooms["status"] == 200
+        @rooms = @response_rooms["data"]
+        @tickets = @response_tickets["data"]
       else
+        @rooms = {}
       end
     end
   end
